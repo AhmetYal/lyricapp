fc3621c78c8dc1d084308843707cb43be8d0e01b
ahmet yalçınkaya <a.yalcinkaya0@gmail.com>
2022-02-01 14:08:33 +0300

    f

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+import { Routes, Route } from "react-router-dom";
+import React from "react";
+import Lyrics from "./components/tracks/Lyrics";
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
       <Navbar />
-    </div>
+      <div className="container">
+        <Routes>
+          <Route exact path="/" element={<Index />} />
+          <Route exact path="/lyrics/track/:id" element={<Lyrics />} />
+        </Routes>
+      </div>
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+
+import { ContextController } from "./context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContextController>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ContextController>
   </React.StrictMode>,
   document.getElementById("root")
 );
